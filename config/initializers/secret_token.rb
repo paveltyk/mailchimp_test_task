d85bebee7d82ca83e112c2c3e9393f0fffadcd82
PavelT <paveltyk@gmail.com>
2010-11-25 12:08:51 +0200

    Rails 3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MailchimpTestTask::Application.config.secret_token = 'f953f93bf221d3d21b278432ed443daf7f1fb58ee7d1caa062f71ddcaa9d924d150fe3d4a74eb816442329aa63c7b5139a6cd684b3d342699dd841d985b5bd40'
