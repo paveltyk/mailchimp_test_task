4ee8edc53d23942f32af8e5de319a3fc7f44ba69
PavelT <paveltyk@gmail.com>
2010-11-25 16:55:25 +0200

    Styles applied

--- config/navigation.rb
+++ config/navigation.rb
@@ -1,7 +1,11 @@
 SimpleNavigation::Configuration.run do |navigation|
+  navigation.selected_class = 'active'
+  
   navigation.items do |primary|
     primary.item :api_key, 'API Key', api_key_path
     primary.item :lists, 'MailChimp Lists', lists_path, :if => lambda { api_key.present? }
     primary.item :users, 'Users', users_path
+
+    primary.dom_class = 'top-navigation'
   end
 end
---
+++ public/javascripts/application.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+  $('input[placeholder], textarea[placeholder]').placeholder();
+});
