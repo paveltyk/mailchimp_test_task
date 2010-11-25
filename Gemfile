cf848acea50173d2f22e274703bf8fd0d4f2bf01
PavelT <paveltyk@gmail.com>
2010-11-25 14:54:42 +0200

    simple-navigation gem added

--- Gemfile
+++ Gemfile
@@ -6,3 +6,4 @@ gem 'bson_ext', '~> 1.1'
 gem 'mongoid', '~> 2.0.0.beta.20'
 gem 'faker', '~> 0.3.1'
 gem "haml-rails"
+gem "simple-navigation"
---
+++ config/navigation.rb
@@ -0,0 +1,6 @@
+SimpleNavigation::Configuration.run do |navigation|
+  navigation.items do |primary|
+    primary.item :api_key, 'API Key', api_key_path
+    primary.item :lists, 'MailChimp Lists', lists_path, :if => lambda { api_key.present? }
+  end
+end
