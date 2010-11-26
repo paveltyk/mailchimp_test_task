49fafc48a97a4dd7efaab2256007d480f87438a1
PavelT <paveltyk@gmail.com>
2010-11-26 10:56:00 +0200

    Added length validation to User model for fname, lname and email fields.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,9 @@ class User
   validates_uniqueness_of :email
   validates_presence_of :email, :fname, :lname
   validates_format_of :email, :with => /^[A-Z0-9_\.%\+\-]+@(?:[A-Z0-9\-]+\.)+(?:[A-Z]{2,4}|museum|travel)$/i
-
+  validates_length_of :email, :maximum => 30
+  validates_length_of :fname, :maximum => 30
+  validates_length_of :lname, :maximum => 30
 
   def build_mailchimp_member
     MailChimp::Member.new email, 'FNAME' => fname, 'LNAME' => lname
